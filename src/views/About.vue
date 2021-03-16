49b98dff540dde0ebc42f139b37eaf1fb779d6d9
kazukikurimoto <kurik6548@gmail.com>
2021-03-16 16:03:24 +0900

    first-commit

---
+++ src/views/About.vue
@@ -0,0 +1,17 @@
+<script>
+import axios from "axios";
+export default {
+  async created() {
+    const item = await axios.get(
+      `https://apis.postcode-jp.com/api/v4=${this}&units=metric&appid=pqwM3Gcq2bmiysVFgSb49oOX12AuPT3yeJIC05Q`
+    );
+      const weatherData = item.data;
+    this.name = weatherData.name;
+    this.main = weatherData.weather[0].main;
+    this.temp = weatherData.main.temp;
+    this.humidity = weatherData.main.humidity;
+    this.speed = weatherData.wind.speed;
+  },
+}
+
+</script>
---
+++ src/views/Home.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <input type="text" v-model="zipcode" maxlength="7" />
+    <button @click="created">住所自動入力</button>  
+    <p>Address:{{addressData}}</p>
+</div>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+  data() {
+    return {
+      zipcode: null,
+      addressData: "",
+    };
+  },
+  methods: {
+    async created() {
+      const response =await axios.get (
+       `https://apis.postcode-jp.com/api/v4/postcodes/{{this.zipcode}}?apiKey=Uq8U252RKyTgec4pbyeLqa4DANzerj0LcSa4N6q`
+        );
+        this.addressData=response
+    }
+  }
+};
+</script>
